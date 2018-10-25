c6c06ada1fa68030ef5e04e91a082c8ca733a27b
Krystian Dziuk <dziukers@gmail.com>
2018-10-25 15:35:49 +0200

    Updates

---
+++ precache-manifest.38b759e098b400799c6a8684ddd2c3f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d668a0208d7e4c263b3301f73b546ea0",
+    "url": "/Image-Recognition-App/static/media/brain.d668a020.png"
+  },
+  {
+    "revision": "a719bffc7f897565cb62",
+    "url": "/Image-Recognition-App/static/js/runtime~main.a719bffc.js"
+  },
+  {
+    "revision": "7d1f718758dfe33e6b2c",
+    "url": "/Image-Recognition-App/static/js/main.7d1f7187.chunk.js"
+  },
+  {
+    "revision": "50a49811127e98bd6ea8",
+    "url": "/Image-Recognition-App/static/js/1.50a49811.chunk.js"
+  },
+  {
+    "revision": "7d1f718758dfe33e6b2c",
+    "url": "/Image-Recognition-App/static/css/main.e9f87a65.chunk.css"
+  },
+  {
+    "revision": "50a49811127e98bd6ea8",
+    "url": "/Image-Recognition-App/static/css/1.dedb8c5b.chunk.css"
+  },
+  {
+    "revision": "5544170d2ef508c213d482f683c81b34",
+    "url": "/Image-Recognition-App/index.html"
+  }
+];
