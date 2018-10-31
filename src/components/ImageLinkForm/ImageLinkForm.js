da87986fb30f9888315ba917dc646706b1b0069e
Krystian Dziuk <dziukers@gmail.com>
2018-10-31 01:38:00 +0100

    fix

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react'
 import './ImageLinkForm.css';
 
 class ImageLinkForm extends Component {
-    constructor(){
-        super();
-    }
-
   render() {
       const {onInputChange, onButtonSubmit, handleRandomLink, value, imageLoading} = this.props;
     return (
